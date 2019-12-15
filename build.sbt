8bc03ef6a5adf5e6b7e3ed020ebce030e0087fb8
mario <mario.callisto@gmail.com>
2019-12-15 16:17:52 +0100

    Initial version

--- build.sbt
+++ build.sbt
@@ -4,8 +4,14 @@ name := "app"
 
 scalaVersion := "2.12.8"
 
+resolvers += Resolver.bintrayRepo("oyvindberg", "ScalablyTyped")
+
 libraryDependencies += "me.shadaj" %%% "slinky-native" % "0.6.3"
 libraryDependencies += "me.shadaj" %%% "slinky-hot" % "0.6.3"
+libraryDependencies += ScalablyTyped.A.`ant-design__react-native`
+libraryDependencies += ScalablyTyped.A.`ant-design__icons-react-native`
+libraryDependencies += ScalablyTyped.B.`bang88__react-native-drawer-layout`
+libraryDependencies += ScalablyTyped.A.`antd-native-slinky-facade`
 
 scalacOptions += "-P:scalajs:sjsDefinedByDefault"
 
--- src/main/scala/hello/world/App.scala
+++ src/main/scala/hello/world/App.scala
@@ -2,47 +2,97 @@ package hello.world
 
 import slinky.core._
 import slinky.core.annotations.react
-
+import slinky.core.facade.Hooks._
 import slinky.native._
 
 import scala.scalajs.js
+import scala.scalajs.js.|
 import scala.scalajs.js.Dynamic.literal
-import scala.scalajs.js.annotation.{JSImport, ScalaJSDefined}
 
-@react class App extends Component {
+import typings.antdDashNative.AntdNativeFacade.{Button, _}
+import typings.react.ScalableSlinky._
+import typings.atAntDashDesignIconsDashReactDashNative.libFillMod.IconFillProps
+import typings.atAntDashDesignIconsDashReactDashNative.libOutlineMod.IconOutlineProps
+import typings.atAntDashDesignReactDashNative.PartialLocale
+import typings.atAntDashDesignReactDashNative.libModalPropsTypeMod.Action
+import typings.atBang88ReactDashNativeDashDrawerDashLayout.atBang88ReactDashNativeDashDrawerDashLayoutMod.{DrawerLayout, default}
+
+@react object App {
+
   type Props = Unit
-  type State = Int
 
-  def initialState = 0
+  val component = FunctionalComponent[Props] { _ =>
+    val (isModalVisible, updateIsModalVisible) = useState(false)
 
-  def render() = {
-    View(
-      style = literal(
-        backgroundColor = "white",
-        padding = 50,
-        flex = 1,
-        flexDirection = "column",
-        justifyContent = "center",
-        alignItems = "center"
-      )
-    )(
-      Image(
-        source = ImageURISource(
-          uri = "https://raw.githubusercontent.com/shadaj/slinky/master/logo.png"
-        ),
-        style = literal(
-          width = 250,
-          height = 250
-        ),
-        resizeMode = "cover"
+    var ref: default | Null = new default {}
+
+    val menus = (1 to 20).map(i => ListItem(ListItemProps())("Menu " + i).withKey(i.toString))
+
+    Provider(ProviderProps(locale = PartialLocale(locale = "enUS")))(
+      Drawer(DrawerProps(
+        drawerRef = (ref = _),
+        sidebar = ScrollView(List(ListProps())(menus)).toST)
+      )(
+        ScrollView(
+          automaticallyAdjustContentInsets = false,
+          showsHorizontalScrollIndicator = false,
+          showsVerticalScrollIndicator = false
+        )(
+          List(ListProps(renderHeader = Text()("List header").toST))(
+            ListItem(ListItemProps(
+              arrow = antdStrings.horizontal,
+              onPress = _ => ref.asInstanceOf[DrawerLayout].openDrawer()))("Open drawer"),
+            ListItem(ListItemProps(
+              arrow = antdStrings.horizontal,
+              onPress = _ => updateIsModalVisible(true)))("Open modal"),
+            ListItem(ListItemProps(
+              arrow = antdStrings.horizontal,
+              onPress = _ => antdToast.success("Successful!")))("Launch success toast")
+          ),
+          View(
+            style = literal(
+              backgroundColor = "white",
+              flex = 1,
+              flexDirection = "column",
+              justifyContent = "center",
+              alignItems = "center"
+            )
+          )(
+            InputItem(InputItemProps(placeholder = "input text")),
+            InputItem(InputItemProps(
+              `type` = antdStrings.password,
+              placeholder = "password",
+              error = true,
+              onErrorClick = _ => antdToast.fail("Always wrong!"),
+              last = true
+            )),
+            WingBlank(WingBlankProps(size = antdStrings.lg))(
+              Button(ButtonProps(
+                onPress = _ => antdToast.fail("Failure!"),
+                `type` = antdStrings.primary
+              ))("Launch fail toast")
+            ),
+            WhiteSpace(WhiteSpaceProps(size = antdStrings.xl)),
+            Text(
+              style = literal(fontSize = 30, color = "red")
+            )("Hello, Slinky Native!"),
+            IconFill(IconFillProps(name = "flag", size = 20))(),
+            IconOutline(IconOutlineProps(name = "gift"))(),
+            Icon(IconProps(name = "experiment", size = antdStrings.lg, color = "#A82"))()
+          )
+        )
       ),
-      Text(
-        style = literal(fontSize = 30, color = "red")
-      )("Hello, Slinky Native!"),
-      Text("Count: " + state),
-      Button(title = "Tap Me!", onPress = () => {
-        setState(_ + 1)
-      })
+      Modal(ModalProps(
+        visible = isModalVisible,
+        transparent = true,
+        maskClosable = true,
+        closable = false,
+        title = "Basic modal",
+        onClose = () => updateIsModalVisible(false),
+        footer = js.Array(
+          Action("Cancel", () => updateIsModalVisible(false), ""),
+          Action("OK", () => updateIsModalVisible(false), ""))
+      ))(Text()("Some contents..."))
     )
   }
 }
